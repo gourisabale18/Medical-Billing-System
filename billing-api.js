680bb5143db8b6a35cdf24ea3b2accfd76299c7e
csuftitan <csuftitan@MacBook-Air-58.lan>
2023-02-13 15:03:40 -0800

    added unit test cases for Billing API with some extra code changes

--- billing-api.js
+++ billing-api.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3001;
+//const port = 3002;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -31,18 +31,64 @@ let bills= [
 ];
 
 //HTTP Post request to create new medical bill which accepts json data
-app.post('/create', function(req, res){
+
+
+    app.post('/create', function(req, res){
+    
+    if(req.body.forEach(element => {
+        if(!element.patient_name)
+    {
+        res.status(400).json({result:'Pass patient_name while creating bill'});
+        return;
+    }
+     }));
+
+     if(req.body.forEach(element => {
+        if(!element.patient_address)
+    {
+        res.status(400).json({result:'Pass patient_address while creating bill'});
+        return;
+    }
+}));
+
+if(req.body.forEach(element => {
+    if(!element.hospital_name)
+{
+    res.status(400).json({result:'Pass hospital_name while creating bill'});
+    return;
+}
+}));
+if(req.body.forEach(element => {
+    if(!element.date_of_service)
+{
+    res.status(400).json({result:'Pass date_of_service while creating bill'});
+    return;
+}
+}));
+if(req.body.forEach(element => {
+    if(!element.bill_amount)
+{
+    res.status(400).json({result:'Pass bill_amount while creating bill'});
+    return;
+}
+}));
+
+
 
     bills.push(req.body);
    console.log('request =' + req.body);
-    res.sendStatus(200);
+   // res.sendStatus(200);
+    res.status(200).json({result:"Medical Bill is created sucessfully"});
 
 });
 
 //HTTP GET request to display medical bills in the form of json array of objects
 
- app.get('/get' ,function(req, res)
+app.get('/get' ,function(req, res)
  {
     return res.json( bills);
- });
- app.listen(3001);
+ })
+
+ module.exports = app.listen(3004);
+ //app.listen(port);
+ 
